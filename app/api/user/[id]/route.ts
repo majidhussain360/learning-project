9aba738b825abef5d2e06860d78677671293882d
majidhussain360 <74454458+majidhussain360@users.noreply.github.com>
2025-09-08 21:16:56 +0500

    dynamin routing updated

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -3,26 +3,13 @@ import { authOptions } from '../../auth/[...nextauth]/options';
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, context: { params: { id: string } }) {
     try {
-        // const token = request.headers.get("Authorization")?.replace("Bearer ", "");
-        // if (!token) {
-        //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-        // }
-
-       
-        
-        // try {
-        //     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string; roleId: string };
-        // } catch (err) {
-        //     return NextResponse.json({ message: "Invalid or expired token" }, { status: 401 });
-        // }
         const session = await getServerSession(authOptions);
         if (!session) {
             return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
         }
-
-        const userId = params.id;
+        const userId = context.params.id;
         const user = await UserService.getUserById(userId);
         return NextResponse.json(user, { status: 200 });
     } catch (error: any) {
@@ -30,25 +17,13 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
     }
 }
 
-export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+export async function PATCH(request: NextRequest, context: { params: { id: string } }) {
     try {
-        // const token = request.headers.get("Authorization")?.replace("Bearer ", "");
-        // if (!token) {
-        //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-        // }
-        
-        // try {
-        //     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string; roleId: string };
-        // } catch (err) {
-        //     return NextResponse.json({ message: "Invalid or expired token" }, { status: 401 });
-        // }
         const session = await getServerSession(authOptions);
         if (!session) {
             return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
         }
-
-
-        const userId = params.id;
+        const userId = context.params.id;
         const { name, username, email } = await request.json();
         const updatedUser = await UserService.updateUser(userId, { name, username, email });
         return NextResponse.json(updatedUser, { status: 200 });
@@ -57,25 +32,13 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     }
 }
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
     try {
-        // const token = request.headers.get("Authorization")?.replace("Bearer ", "");
-        // if (!token) {
-        //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-        // }
-        
-        // try {
-        //     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string; roleId: string };
-        // } catch (err) {
-        //     return NextResponse.json({ message: "Invalid or expired token" }, { status: 401 });
-        // }
         const session = await getServerSession(authOptions);
         if (!session) {
             return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
         }
-
-
-        const userId = params.id;
+        const userId = context.params.id;
         await UserService.deleteUser(userId);
         return NextResponse.json({ message: "User deleted successfully" }, { status: 200 });
     } catch (error: any) {
